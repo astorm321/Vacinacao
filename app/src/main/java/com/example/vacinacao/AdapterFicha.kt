a5f4cb7909e210dc9ef02464250af4711d982f88
Vitor Pereira <vitor123@live.com.pt>
2021-07-04 22:17:06 +0100

    Criaçao ficheiro adapter ( imcompleto )
    criaçao do layour fragment para as fichas ( lista novo elimina e altera)

---
+++ app/src/main/java/com/example/vacinacao/AdapterFicha.kt
@@ -0,0 +1,12 @@
+package com.example.vacinacao
+
+import android.database.Cursor
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterFicha val fragment: ListaFichaFragment) : RecyclerView.Adapter<AdapterFicha.ViewHolderFicha>()  {
+
+
+}
